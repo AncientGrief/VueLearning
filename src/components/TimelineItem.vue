204878fff2ad7c8bba64f52588299d56282ee773
AncientGrief <marcel.meisner@gmail.com>
2023-10-14 18:08:16 +0200

    Init

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { TimelinePost } from '../posts';
+import { DateTime } from 'luxon';
+
+defineProps<{
+    post: TimelinePost;
+}>()
+</script>
+
+<template>
+    <a class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
+        <a>{{ post.title }}</a>
+        <div>{{ post.created?.toLocaleString(DateTime.DATE_FULL) }}</div>
+    </a>
+</template>
